926f00e54e4b5ed30c4c40b584637845c0b740f1
Aneesha Gupta <aneeshag@usc.edu>
2014-12-09 18:18:17 -0800

    update update.php

--- FinalProject/update.php
+++ FinalProject/update.php
@@ -16,17 +16,30 @@ if(mysqli_connect_errno()){
     echo "Connection failed: ". mysqli_connct_error();
 }
 
+$face_height = $_REQUEST['faceHeight'];
+$face_width = $_REQUEST['faceWidth'];
+$face_score = $face_height/$face_width; //bigger the number, bigger the sunglasses
+$face_constant = 1;
+$face_type = null;
+if($face_score > 1) {
+    $face_type = "Oval";
+}
+else if ($face_score < 1) {
+    $face_type = "Round";
+}
+else
+    $face_type = "Square";
 
 $update_sql = "UPDATE measurements
                 SET eyeSize = '" . $_REQUEST['eyeSize'] . "', faceWidth = '" . $_REQUEST['faceWidth'] .
-    " ', faceHeight = '". $_REQUEST['faceHeight'] . "' WHERE username LIKE '%" .$user ."%'";
+    " ', faceHeight = '". $_REQUEST['faceHeight'] . "' , faceType = '". $face_type ."' WHERE username LIKE '%" .$user ."%'";
 
 $update_results = mysqli_query($conn, $update_sql);
-
+$userName = $user;
 if(!$update_results){
     exit("Update SQL Error: " . mysqli_error($conn));
 } else  {
-   header("Location: profile.php?user=".$user);
+    header("Location: profile.php?userName=".$userName);
 }
 
 
