59abf20d0bf0248e930200c86d6adc22ee68269f
Aneesha Gupta <aneeshag@usc.edu>
2014-12-16 12:23:06 -0800

    initial changes

--- FinalProject/insert_info.php
+++ FinalProject/insert_info.php
@@ -1,30 +1,27 @@
 <?php
 
-
 $conn = mysqli_connect("uscitp.com", "aneeshag_final", "Final123", "aneeshag_finalproject");
 if(mysqli_connect_errno()){
     echo "Connection failed: ". mysqli_connct_error();
 }
-session_start();
-$username = $_SESSION['username'];
 
+
+$username = $_REQUEST['username'];
 $eye_size = $_REQUEST['eye_size'];
 $face_height = $_REQUEST['face_height'];
 $face_width = $_REQUEST['face_width'];
 
-
 $face_score = $face_height/$face_width; //bigger the number, bigger the sunglasses 
 $face_constant = 1;
-$face_type = "Blah";
+$face_type = null;
 if($face_score > 1) {
     $face_type = "Oval";
 }
-
-else {
+else if ($face_score < 1) {
     $face_type = "Round";
 }
-
-echo $face_type;
+else
+    $face_type = "Square";
 
 $sql = "INSERT INTO measurements (userName, eyeSize, faceHeight, faceWidth, faceType)
         VALUES ( '". $username ."', " . $eye_size .", ".$face_height.", ".$face_width.", '".$face_type ."')";
@@ -35,6 +32,13 @@ $results = mysqli_query($conn, $sql);
 if(!$results){
     echo "Error: " . mysqli_error($conn);
 }
+
+else  {
+
+    header("Location: profile.php?userName=".$username);
+}
+
+
 
 ?>
 
